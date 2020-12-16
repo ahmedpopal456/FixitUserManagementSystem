eb7d0124e2b306908ec0d0e601d651ad56149a10
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-12-15 23:37:49 -0500

    (#304) Created UserDocument

--- Fixit.UserManagement.System/Fixit.User.Management.Lib/Mappers/UserManagementMapper.cs
+++ Fixit.UserManagement.System/Fixit.User.Management.Lib/Mappers/UserManagementMapper.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using AutoMapper;
+using Fixit.User.Management.Lib.Models;
+using Fixit.User.Management.Lib.Models.Documents;
 
 namespace Fixit.User.Management.Lib.Mappers
 {
   public class UserManagementMapper : Profile
   {
+    public UserManagementMapper()
+    {
+    }
   }
 }
---
+++ Fixit.UserManagement.System/Fixit.User.Management.Lib/Models/Documents/UserDocument.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Fixit.Core.Database;
+using Fixit.Core.DataContracts.Users;
+using Fixit.Core.DataContracts.Users.Address;
+using Fixit.Core.DataContracts.Users.Documents;
+using Fixit.Core.DataContracts.Users.Enums;
+using Fixit.Core.DataContracts.Users.Ratings;
+
+namespace Fixit.User.Management.Lib.Models.Documents
+{
+  public class UserDocument : DocumentBase
+  {
+    public Guid Id { get; set; }
+
+    public string UserPrincipalName { get; set; }
+
+    public string ProfilePictureUrl { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public UserState State { get; set; }
+
+    public RatingsSummaryDto Rating { get; set; }
+
+    public AddressDto Address { get; set; }
+
+    public UserRole Role { get; set; }
+
+    public Gender Gender { get; set; }
+
+    public UserStatusDto Status { get; set; }
+
+    public string TelephoneNumber { get; set; }
+
+    public long CreatedTimestampsUtc { get; set; }
+
+    public long UpdatedTimestampsUtc { get; set; }
+
+    public IEnumerable<DocumentSummaryDto> Documents { get; set; }
+  }
+}
